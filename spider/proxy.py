529e1ceb523b202c3215c08964a513450105d0c6
zhangyikun <1251134350@qq.com>
2017-11-15 17:33:13 +0800

    改写proxy，获取普通ip

--- spider/proxy.py
+++ spider/proxy.py
@@ -4,15 +4,21 @@
 Description
 - 获取IP代理。
 Info
-- author : "moran"
-- github : "moranzcw@gmail.com"
-- date   : "2017.7.29"
+- author : "zyk"
+- github : "1251134350@qq.com"
+- date   : "2017.11.15"
 """
-__author__ = """\
-  /\/\   ___  _ __ __ _ _ __  
- /    \ / _ \| '__/ _` | '_ \ 
-/ /\/\ \ (_) | | | (_| | | | |
-\/    \/\___/|_|  \__,_|_| |_|"""
+__author__ = "zyk"
+
+from bs4 import  BeautifulSoup
+import  requests
+import  random
+
+url = 'http://www.xicidaili.com/nn/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+}
+
 
 # 代理服务器
 proxyHost = "http-dyn.abuyun.com"
@@ -25,8 +31,8 @@ proxyPass = ""
 proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
     "host": proxyHost,
     "port": proxyPort,
-    "user": proxyUser,
-    "pass": proxyPass,
+    #"user": proxyUser,
+    #"pass": proxyPass,
 }
 
 proxies = {
@@ -34,5 +40,35 @@ proxies = {
     "https": proxyMeta,
 }
 
-def getproxies():
+def get_ip_list(url, headers):
+    web_data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(web_data.text, 'html.parser')
+    ips = soup.find_all('tr')
+    ip_list = []
+    for i in range(1, len(ips)):
+        ip_info = ips[i]
+        print(ip_info)
+        tds = ip_info.find_all('td')
+        ip_list.append(tds[1].text + ':' + tds[2].text)
+    return ip_list
+
+def get_random_ip(ip_list):
+    proxy_list = []
+    for ip in ip_list:
+        proxy_list.append('http://' + ip)
+    proxy_ip = random.choice(proxy_list)
+    proxies['http'] = proxy_ip
+    proxies['https'] = proxy_ip
     return proxies
+
+def getproxies():
+    ip_list = get_ip_list(url, headers=headers)
+    proxies = get_random_ip(ip_list)
+    return proxies
+
+if __name__ == '__main__':
+
+    print(getproxies())
+
+
+
